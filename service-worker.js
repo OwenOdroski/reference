821cdd26db3593b1f4e48f1420d01a8ec8ddda07
OwenOdroski <116469702+OwenOdroski@users.noreply.github.com>
2026-01-19 02:45:53 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,88 @@
-const CACHE_NAME = "my-cache-v1";
-
-// List of assets to precache
-const PRECACHE_ASSETS = [
-  '/reference/',
-  '/reference/index.html',
-  '/reference/style.css',
-  '/reference/script.js',
-  '/reference/db.json',
-  '/reference/f16.glb',
-  '/reference/three.js',
-  '/reference/Orbit.js',
-  '/reference/Loader.js',
-  '/reference/781a.png',
-  '/reference/781H-images-0.jpg',
-  '/reference/781H-images-1.jpg',
-  '/reference/icon.jpg',
-  ''
-];
-
-// Install event - cache files
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(PRECACHE_ASSETS);
-    })
-  );
-  self.skipWaiting();
+const version = "v0.1"
+if (!self.define) {
+    let e, s = {};
+    const i = (i, n) => (i = new URL(i + ".js",n).href,
+    s[i] || new Promise(s => {
+        if ("document"in self) {
+            const e = document.createElement("script");
+            e.src = i,
+            e.onload = s,
+            document.head.appendChild(e)
+        } else
+            e = i,
+            importScripts(i),
+            s()
+    }
+    ).then( () => {
+        let e = s[i];
+        if (!e)
+            throw new Error(`Module ${i} didn’t register its module`);
+        return e
+    }
+    ));
+    self.define = (n, t) => {
+        const r = e || ("document"in self ? document.currentScript.src : "") || location.href;
+        if (s[r])
+            return;
+        let o = {};
+        const l = e => i(e, r)
+          , c = {
+            module: {
+                uri: r
+            },
+            exports: o,
+            require: l
+        };
+        s[r] = Promise.all(n.map(e => c[e] || l(e))).then(e => (t(...e),
+        o))
+    }
+}
+define(["./workbox"], function(e) {
+    "use strict";
+    self.addEventListener("message", e => {
+        e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting()
+    }
+    ),
+    e.precacheAndRoute([{
+        url: "/index.html",
+        revision: version
+    }, {
+        url: "/Loader.js",
+        revision: version
+    }, {
+        url: "/781a.png",
+        revision: version
+    }, {
+        url: "/781H-images-0.jpg",
+        revision: version
+    }, {
+        url: "/781H-images-1.jpg",
+        revision: version
+    }, {
+        url: "/f16.glb",
+        revision: version
+    }, {
+        url: "/script.js",
+        revision: version
+    }, {
+        url: "/style.css",
+        revision: version
+    }, {
+        url: "/three.js",
+        revision: version
+    }, {
+        url: "/db.json",
+        revision: version
+    }, {
+        url: "/icon.jpg",
+        revision: version
+    }, {
+        url: "/Orbit.js",
+        revision: version
+    }, {
+        url: "/manifest.json",
+        revision: version
+    }], {}),
+    e.cleanupOutdatedCaches(),
+    e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))
 });
-
-// Activate event - cleanup old caches
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      caches.match('/CC/index.html')
-        .then((resp) => resp || caches.match('/CC/offline.html'))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then((resp) => resp)
-    );
-  }
-});
-
